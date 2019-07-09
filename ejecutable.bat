077b8cbefdcee51838e2bdb2d19a2e25d37ff38a
gonzaloGMC <gonzalo.morenoc@vass.es>
2019-07-09 08:39:33 +0200

    updated

--- ejecutable.bat
+++ ejecutable.bat
@@ -6,4 +6,4 @@ set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 
 set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%" 
 
-call newman run "C:\Program Files (x86)\Jenkins\workspace\poc_api_git\POC_API.postman_collection.json" -e "C:\Program Files (x86)\Jenkins\workspace\poc_api_git\poc.postman_environment.json" -r cli,html,junit --reporter-html-export C:\Program Files (x86)\Jenkins\workspace\poc_api_git\reportsHtml\report_%fullstamp%.html --reporter-junit-export C:\Program Files (x86)\Jenkins\workspace\poc_api_git\reportsJunit\reportJunit_%fullstamp%.xml 
+call newman run "C:\Program Files (x86)\Jenkins\workspace\poc_api_git\POC_API.postman_collection.json" -e "C:\Program Files (x86)\Jenkins\workspace\poc_api_git\poc.postman_environment.json" -r cli,html,junit --reporter-html-export "C:\Program Files (x86)\Jenkins\workspace\poc_api_git\reportsHtml\report_%fullstamp%.html" --reporter-junit-export "C:\Program Files (x86)\Jenkins\workspace\poc_api_git\reportsJunit\reportJunit_%fullstamp%.xml" 
