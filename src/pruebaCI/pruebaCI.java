e372d3ee6be9f717f81525e8a3c5137864ca41ec
SE07781 <gmoreno_vass@bankinter.com>
2019-08-09 10:33:41 +0200

    issue 1

--- src/pruebaCI/pruebaCI.java
+++ src/pruebaCI/pruebaCI.java
@@ -4,6 +4,8 @@ public class pruebaCI {
 
 	public static void main(String[] args) {
 		System.out.println("Esta es una prueba de CI");
+		
+		System.out.println("Esta es una prueba de dfdfdfdCI");
 
 	}
 
